41ba0d42698c1b0e964ba58274f97d7af8fb6884
gogh21 <gogh.phy@gmail.com>
2022-12-05 18:19:22 +0900

    feat: solve problem3

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,23 @@
 function problem3(number) {
   var answer;
+  let check = 0;
+  for (let num = 1; num <= number; num++) {
+    let changeString = num.toString();
+    let threeArray = changeString.match(/3/g);
+    let sixArray = changeString.match(/6/g);
+    let nineArray = changeString.match(/9/g);
+
+    if (threeArray != null) {
+      check += threeArray.length;
+    }
+    if (sixArray != null) {
+      check += sixArray.length;
+    }
+    if (nineArray != null) {
+      check += nineArray.length;
+    }
+  }
+  answer = check;
   return answer;
 }
-
 module.exports = problem3;
