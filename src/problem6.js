a5b2a185ae79ef22f824c9e97e829cf0d5e23ae3
gogh21 <gogh.phy@gmail.com>
2022-12-07 03:11:18 +0900

    feat: solve problem6

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,36 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  var answer = [];
+  let nickNameObj = {};
+
+  for (form of forms) {
+    let emailAdress = form[0];
+    let nickName = form[1];
+
+    for (let ind = 0; ind < nickName.length; ind++) {
+      sliceName = nickName.substr(ind, ind+2);
+      if (sliceName.length === 2) {
+        if (sliceName in nickNameObj) {
+          nickNameObj[sliceName] += 1;
+        } else {
+          nickNameObj[sliceName] = 1;
+        }
+      }
+    }
+  }
+
+  let pairs = Object.entries(nickNameObj);
+  pairs.sort((a, b) => b[1] - a[1]);
+  let sortedObj = Object.fromEntries(pairs);
+  let maxString = pairs[0][0];
+
+  for (f of forms) {
+    let email = f[0];
+    let name = f[1];
+    if (name.includes(maxString)) {
+      answer.push(email);
+    }
+  }
+  return answer.sort();
 }
 
 module.exports = problem6;
