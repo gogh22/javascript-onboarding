708fce0a72859713923f45cacfa1d9bfdd384ba8
gogh21 <gogh.phy@gmail.com>
2022-12-06 22:28:53 +0900

    feat: solve problem5

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,23 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  var answer = [];
+
+  const coins = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  let numCoins = {};
+
+  for (let i = 0; i < coins.length; i++) {
+    let coin = coins[i];
+
+    let times = Math.floor(money/coin);
+    numCoins[coin] = times;
+
+    money = money % coin;
+  }
+
+for (let numsCoinsKey in numCoins) {
+  answer.push(numCoins[numsCoinsKey]);
+}
+  return answer.reverse();
 }
 
 module.exports = problem5;
